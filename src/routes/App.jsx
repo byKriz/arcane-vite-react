09582e8b9d624a9a95fdbad1edcd3b4efca93926
byKriz <cristianesquivelh@hotmail.com>
2022-08-02 16:13:31 -0400

    importando el header a App

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
+import { Header } from "../container/Header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />} />
