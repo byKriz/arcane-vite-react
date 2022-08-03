2d2277dbaeb1b1ad8a5b5e092aee847b1fff5683
byKriz <cristianesquivelh@hotmail.com>
2022-08-03 09:04:53 -0400

    ajustando el responsive del display

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -3,28 +3,32 @@ import styles from "./Menu.module.scss";
 
 export function Menu() {
   const [slide, setSlide] = useState(false);
-    const handleSlide = () => {
-        setSlide(!slide);
-    }
+  const handleSlide = () => {
+    setSlide(!slide);
+  };
 
   const LINKS = [
-    {name: "Home", link: "/"},
-    {name: "Characters", link: "/"},
-    {name: "Locations", link: "/"},
-    {name: "Social", link: "/"},
-  ]
+    { name: "Home", link: "/" },
+    { name: "Characters", link: "/" },
+    { name: "Locations", link: "/" },
+    { name: "Social", link: "/" },
+  ];
 
   return (
     <>
       <div className={styles.menu}>
-        <div className={styles.menuIcon} onClick={handleSlide}></div>
+        <div className={slide ? `${styles.menuIcon} ${styles.deactivete}` : `${styles.menuIcon}`} onClick={handleSlide}></div>
       </div>
-      <nav className={slide ? `${styles.navMenuItems} ${styles.activate}` : styles.navMenuItems}>
+      <nav
+        className={
+          slide ? `${styles.navMenuItems} ${styles.activate}`: `${styles.navMenuItems}`
+        }
+      >
         <ul className={styles.menuContainer}>
           {LINKS.map((link) => (
             <li>
-            <a href={link.link}>{link.name}</a>
-          </li>
+              <a href={link.link}>{link.name}</a>
+            </li>
           ))}
         </ul>
       </nav>
