b7782931fda6338cf8953ce5159989729bd1d3f2
byKriz <cristianesquivelh@hotmail.com>
2022-08-03 10:01:30 -0400

    agregando la capa de contraste

--- src/components/VideoBG/VideoBG.jsx
+++ src/components/VideoBG/VideoBG.jsx
@@ -1,10 +1,17 @@
 import style from "./VideoBG.module.scss";
 
-
 export const VideoBG = ({ videoBG }) => {
   return (
     <>
-      <video className={style.videoSlide} src={videoBG} autoPlay muted loop></video>
+      <div className={style.screen}>
+      </div>
+      <video
+          className={style.videoSlide}
+          src={videoBG}
+          autoPlay
+          muted
+          loop
+        ></video>
     </>
   );
 };
