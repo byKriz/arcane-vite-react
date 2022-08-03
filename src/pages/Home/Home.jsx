76f4542bade587c807a885da12c66287c2a70071
byKriz <cristianesquivelh@hotmail.com>
2022-08-03 10:35:01 -0400

    ntegrando el componente synopsis

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import { VideoBG } from "../../components/VideoBG/VideoBG";
 import video from "../../assets/videos/videoBackground.mp4";
+import { Synopsis } from "../../components/Synopsis/Synopsis";
 
 export const Home = () => {
   return (
     <>
-      <VideoBG videoBG={video}/>
+      <Synopsis>
+        <VideoBG videoBG={video} />
+      </Synopsis>
     </>
   );
 };
