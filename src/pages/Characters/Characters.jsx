f1ceaec32917488b12505e1b0b6e199f6ba44107
byKriz <cristianesquivelh@hotmail.com>
2022-08-05 13:07:53 -0400

    implementando en la pagina

--- src/pages/Characters/Characters.jsx
+++ src/pages/Characters/Characters.jsx
@@ -1,13 +1,27 @@
+import { Card } from "../../components/Card/Card";
+import { CardsContainer } from "../../components/CardsContainer/CardsContainer";
 import { Layout } from "../../container/Layout/Layout";
 import styles from "./Characters.module.scss";
+import jinx from "../../assets/images/Jinx.png";
+import vi from "../../assets/images/Vi.png";
 
 export function Characters() {
   return (
     <>
-        <Layout>
-            {/* <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui nisi, sint voluptates beatae aut ducimus inventore nesciunt enim cum cumque voluptas officiis? Porro earum magni nihil reprehenderit reiciendis sint sunt!</p> */}
-        </Layout>
-      <div>Characters</div>
+      <Layout>
+        <CardsContainer>
+          <Card
+            image={jinx}
+            name="Jinx"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae a earum praesentium, sequi esse nesciunt, animi assumenda obcaecati veritatis id ex! Dicta a quae earum ipsa rerum animi impedit adipisci."
+          />
+          <Card
+            image={vi}
+            name="Vi"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae a earum praesentium, sequi esse nesciunt, animi assumenda obcaecati veritatis id ex! Dicta a quae earum ipsa rerum animi impedit adipisci."
+          />
+        </CardsContainer>
+      </Layout>
     </>
   );
 }
