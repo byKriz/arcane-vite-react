b8244ce9f43edd5383e21d150b2974442218b09c
byKriz <cristianesquivelh@hotmail.com>
2022-08-05 12:29:48 -0400

    creando la pagina characters

---
+++ src/pages/Characters/Characters.jsx
@@ -0,0 +1,13 @@
+import { Layout } from "../../container/Layout/Layout";
+import styles from "./Characters.module.scss";
+
+export function Characters() {
+  return (
+    <>
+        <Layout>
+            {/* <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui nisi, sint voluptates beatae aut ducimus inventore nesciunt enim cum cumque voluptas officiis? Porro earum magni nihil reprehenderit reiciendis sint sunt!</p> */}
+        </Layout>
+      <div>Characters</div>
+    </>
+  );
+}
