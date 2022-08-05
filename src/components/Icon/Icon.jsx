0e56f232e3eae52f4595536ef16e0b8471cc7c31
byKriz <cristianesquivelh@hotmail.com>
2022-08-04 23:51:53 -0400

    ajustando los tamaños

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -2,22 +2,25 @@
 import styles from "./Icon.module.scss";
 
 export function Icon({ link, iconName }) {
+  const github = "fa-brands fa-github-alt";
+  const twitter = "fa-brands fa-twitter";
+
   if (iconName === "github") {
     return (
       <a href={link} className={styles.iconContainer} target="_blank">
-        <i className="fa-brands fa-github-alt"></i>
+        <i className={`${github} ${styles.icon}`}></i>
       </a>
     );
   } else if (iconName === "twitter") {
     return (
       <a href={link} className={styles.iconContainer} target="_blank">
-        <i class="fa-brands fa-twitter"></i>
+        <i class={`${twitter} ${styles.icon}`}></i>
       </a>
     );
   }
   return (
     <a href={link} className={styles.iconContainer} target="_blank">
-      <i className="fa-brands fa-github-alt"></i>
+      <i className={`${github} ${styles.icon}`}></i>
     </a>
   );
 }
