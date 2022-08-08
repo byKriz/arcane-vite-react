b9f23a9150c5e83330564ab4996b56c5048f5ab1
byKriz <cristianesquivelh@hotmail.com>
2022-08-08 14:28:44 -0400

    creando el nuevo diseño de cartas

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,10 +3,13 @@ import styles from "./Card.module.scss";
 export function Card({ image, name, description }) {
   return (
     <div className={styles.card}>
-      <div className={styles.imgBx}>
-        <img src={image} />
+      <div className={styles.imageContent}>
+        <span className={styles.overlay}></span>
+        <div className={styles.cardImage}>
+          <img src={image} alt={name} className={styles.cardImg}/>
+        </div>
       </div>
-      <div className={styles.content}>
+      <div className={styles.cardContent}>
         <h2>{name}</h2>
         <p>{description}</p>
       </div>
